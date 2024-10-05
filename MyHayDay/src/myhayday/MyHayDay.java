a9ad4da842e80fae5a3220fd7d2fda115b8d13c6
pc <pc@LAPTOP-VSB89IV1>
2024-10-05 12:41:38 +0200

    Creación de Trigo, atributos clase e instancia. Closes #3

--- MyHayDay/src/myhayday/MyHayDay.java
+++ MyHayDay/src/myhayday/MyHayDay.java
@@ -8,11 +8,22 @@ package myhayday;
  *
  * @author pc
  */
-public class MyHayDay {
+public class Trigo {
 
     /**
      * @param args the command line arguments
      */
+    public static final String ICONO = "/img/trigo.png";
+    public static final int DURACION = 2;
+    public static final int EXPERIENCIA =1;
+    public static final int PRECIO_VENTA = 2;
+    public static final int RIEGO = 10;
+    
+    private String icono;
+    private int duracionProduccion;
+    private int experienciaRecolecta;
+    private int precioVenta;
+    private int riego;
     public static void main(String[] args) {
         // TODO code application logic here
     }
